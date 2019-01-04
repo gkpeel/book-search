bf9fc2f497b517c7a723a4b0803ef4c057539b33
gkpeel <gkpeel@gmail.com>
2019-01-04 18:14:03 -0500

    added axios to client and server

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+
+  componentDidMount() {
+    console.log('consoleDidMount did run');
+  }
   render() {
     return (
       <div className="App">
